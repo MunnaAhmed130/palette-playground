2c08e72cb01c79c9d76168c3038ad57e605e4041
Munna Ahmed <munnaahmed2025@gmail.com>
2024-01-01 23:00:20 +0600

    commit

--- app.js
+++ app.js
@@ -2,53 +2,10 @@ const generateBtn = document.querySelector(".generator");
 const whiteboard = document.querySelector(".whiteboard");
 const container = document.querySelector(".generator-container");
 const placeholder = document.querySelector(".placeholder");
-const tooltip = document.querySelector("#tooltip");
+const tooltip = document.getElementById("tooltip");
+const boxes = document.querySelectorAll(".box");
 
 let hexGenerated = false;
-
-container.addEventListener("click", (e) => {
-    console.log(e);
-    if (e.target === generateBtn) {
-        // console.log(true);
-        whiteboard.removeChild(whiteboard.firstElementChild);
-
-        if (container.lastElementChild.classList[0] == "copied") {
-            container.removeChild(container.lastElementChild);
-        }
-
-        tooltip.classList.remove("before");
-        tooltip.classList.add("tooltip");
-        tooltip.style.animation = "bounce 1s ease forwards";
-
-        setTimeout(() => {
-            tooltip.style.animation = "";
-            tooltip.classList.add("before");
-            tooltip.classList.remove("tooltip");
-        }, 1000);
-
-        const span = document.createElement("span");
-        span.innerText = generate();
-        whiteboard.appendChild(span);
-        hexGenerated = true;
-    }
-    if (e.target !== generateBtn) {
-        // console.log(false);
-        // console.log(whiteboard.children);
-        if (hexGenerated) {
-            navigator.clipboard.writeText(e.target.innerText);
-
-            if (container.lastElementChild.classList[0] !== "copied") {
-                const span = document.createElement("span");
-                span.innerText = "Copied";
-                span.classList.add("copied");
-                setTimeout(() => {
-                    span.style.opacity = 1;
-                }, 100);
-                container.appendChild(span);
-            }
-        }
-    }
-});
 
 // randome hex color code generator
 function generate() {
@@ -63,26 +20,76 @@ function generate() {
     return value;
 }
 
-const boxes = document.querySelectorAll(".box");
-const body = document.body;
+const changeColorCode = () => {
+    whiteboard.removeChild(whiteboard.firstElementChild);
+
+    if (container.lastElementChild.classList[0] == "copied") {
+        container.removeChild(container.lastElementChild);
+    }
+
+    tooltip.classList.remove("before");
+    tooltip.classList.add("tooltip");
+    tooltip.style.animation = "bounce 1s ease forwards";
+
+    setTimeout(() => {
+        tooltip.style.animation = "";
+        tooltip.classList.add("before");
+        tooltip.classList.remove("tooltip");
+    }, 1000);
+
+    const span = document.createElement("span");
+    span.innerText = generate();
+    whiteboard.appendChild(span);
+
+    hexGenerated = true;
+};
+
+const copyColorCode = (colorCode) => {
+    if (hexGenerated) {
+        navigator.clipboard.writeText(colorCode);
+
+        if (container.lastElementChild.classList[0] !== "copied") {
+            const span = document.createElement("span");
+            span.innerText = "Copied";
+            span.classList.add("copied");
+            setTimeout(() => {
+                span.style.opacity = 1;
+            }, 100);
+            container.appendChild(span);
+        }
+    }
+};
+
+container.addEventListener("click", (e) => {
+    // console.log(e);
+    if (e.target === generateBtn) {
+        changeColorCode();
+    }
+
+    if (e.target !== generateBtn) {
+        copyColorCode(e.target.innerText);
+    }
+});
 
 boxes.forEach((box) => {
-    const span = box.lastElementChild;
-    // console.log(span.innerText);
+    const boxCode = box.querySelector(".color-code");
     box.style.backgroundColor = box.lastElementChild.innerText;
     box.addEventListener("click", (e) => {
-        body.style.backgroundColor = box.lastElementChild.innerText;
+        document.body.style.backgroundColor = boxCode.innerText;
         navigator.clipboard.writeText(e.target.innerText);
     });
+
     box.addEventListener(
         "contextmenu",
         (e) => {
             e.preventDefault();
-            navigator.clipboard.readText().then((text) => {
-                if (text.match(/^#[a-fA-F0-9]{6}$/)) {
-                    span.innerText = text;
-                    box.style.backgroundColor = text;
+            navigator.clipboard.readText().then((code) => {
+                if (code.match(/^#[a-fA-F0-9]{6}$/)) {
+                    boxCode.innerText = code;
+                } else if (text.match(/^[a-fA-F0-9]{6}$/)) {
+                    boxCode.innerText = "#" + code;
                 }
+                box.style.backgroundColor = boxCode.innerText;
             });
         },
         true
